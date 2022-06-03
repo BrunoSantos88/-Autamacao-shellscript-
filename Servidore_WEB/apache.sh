1b895bb85a3aeb3d29903ce0b13a5bedd6451cd6
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2022-06-03 12:11:33 -0300

    alteração de pastas

--- Servidore_WEB/apache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum -y install httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd
-sudo touch /var/www/html/index.html
-sudo chmod 755 /var/www/html/index.html
-sudo echo  > '<html><h1>Bruno Santos re/Start Project Work</h1></html>' > /var/www/html/index.html
